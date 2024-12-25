9a88ce6687b61c5fe50db85241a87eb9202d3d1f
Todd Bruss <todd@starplayrx.com>
2024-12-25 14:59:22 -0500

    hide menu bar added back

--- Sources/GameChanger/GameChanger.swift
+++ Sources/GameChanger/GameChanger.swift
@@ -403,9 +403,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Pre-initialize the cache
         initializeCache()
         
+        let presOptions: NSApplication.PresentationOptions = [.hideDock, .hideMenuBar]
+        NSApp.presentationOptions = presOptions
+
         NSApp.setActivationPolicy(.regular)
         NSApp.activate(ignoringOtherApps: true)
         
+
+
         // Clean window setup code
         DispatchQueue.main.async {
             if let window = NSApp.windows.first {
@@ -420,6 +425,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                     }
                 }
             }
+
+       
         }
     }
     
